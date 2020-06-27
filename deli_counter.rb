e50170256d786375305da00b037cb003db382331
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-06-27 01:59:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,4 +16,11 @@ def line(katz_deli)
   end
 
 def now_serving(katz_deli, name)
+  if katz_deli.length == 0
+      puts "There is nobody waiting to be served!"
+    else
+      puts "Currently serving #{katz_deli.shift}."
+    end
+
+  end
     
